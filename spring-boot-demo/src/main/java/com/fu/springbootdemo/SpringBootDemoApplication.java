834f027b93e8829c2a6e64eb047234b32dafb2c9
Meta39 <53995537@qq.com>
2025-06-26 16:41:32 +0800

    异步线程池和定时任务线程池配置优化

--- spring-boot-demo/src/main/java/com/fu/springbootdemo/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/fu/springbootdemo/SpringBootDemoApplication.java
@@ -2,9 +2,7 @@ package com.fu.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync //开启异步线程池注解
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
--- spring-boot-demo/src/main/java/com/fu/springbootdemo/config/AsyncConfig.java
+++ spring-boot-demo/src/main/java/com/fu/springbootdemo/config/AsyncConfig.java
@@ -1,10 +1,16 @@
 package com.fu.springbootdemo.config;
 
+import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -14,17 +20,27 @@ import java.util.concurrent.ThreadPoolExecutor;
  * 1、异步方法必须是public void
  * 2、确保执行方法和异步执行的方法不在同一个类，如需在同类，需要手动代理。
  */
+@EnableAsync //开启异步线程注解
+@EnableScheduling //开启定时任务注解
+@Slf4j
 @Setter
+@Getter
 @Configuration
 @ConfigurationProperties(prefix = "spring.task.execution.pool") // 该注解的locations已经被启用，现在只要是在环境中，都会优先加载
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
     private int coreSize = 8;//设置默认值，如果yml配置文件有值，则赋值，否则使用默认值。
     private int maxSize = 16;
     private int keepAlive = 60;
     private int queueCapacity = 1000;
+    private String threadNamePrefix = "schedulerTask";
 
-    @Bean("async")
-    public Executor async() {
+    /**
+     * 修改默认的 Async 异步线程池配置
+     * 注意：异步线程池和定时任务线程池的最大线程池大小是大小相同，但不是公用一个线程池，他们是互相独立的。
+     * 只是线程池大小设置成了一样而已。
+     */
+    @Override
+    public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(coreSize);//默认线程池大小
         executor.setMaxPoolSize(maxSize);//最大线程池大小
@@ -39,7 +55,21 @@ public class AsyncConfig {
          */
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
+        log.info("异步线程池大小={}", maxSize);
         return executor;
+    }
+
+    /**
+     * 定时任务线程池配置
+     */
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(maxSize);
+        scheduler.setThreadNamePrefix(threadNamePrefix);
+        scheduler.initialize();
+        log.info("定时任务线程池大小={}", maxSize);
+        return scheduler;
     }
 
 }
