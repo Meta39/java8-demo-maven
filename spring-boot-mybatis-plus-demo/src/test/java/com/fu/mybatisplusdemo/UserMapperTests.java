b5799dae74d5dfbefb0d780101d25f1396b1e91d
Meta39 <53995537@qq.com>
2025-08-20 11:53:06 +0800

    新增：MyBatisPlusUtils工具类，可以实现分批查询全部数据，用于解决ORACLE IN 超过1000条的问题

--- spring-boot-mybatis-plus-demo/src/test/java/com/fu/mybatisplusdemo/UserMapperTests.java
+++ spring-boot-mybatis-plus-demo/src/test/java/com/fu/mybatisplusdemo/UserMapperTests.java
@@ -1,14 +1,20 @@
 package com.fu.mybatisplusdemo;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.fu.mybatisplusdemo.entity.User;
 import com.fu.mybatisplusdemo.enums.SexEnum;
 import com.fu.mybatisplusdemo.mapper.UserMapper;
+import com.fu.mybatisplusdemo.util.MyBatisPlusUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+@Slf4j
 @SpringBootTest
 public class UserMapperTests {
     @Autowired
@@ -26,4 +32,18 @@ public class UserMapperTests {
         user.setIsDeleted(0);
         System.out.println(this.userMapper.insert(user));
     }
+
+    @Test
+    public void myBatisPlusUtilsFindByPageTest() {
+        List<User> allUsers = new ArrayList<>();
+        MyBatisPlusUtils.findByPage(userMapper,
+                new LambdaQueryWrapper<User>().like(User::getName, "M"),
+                users -> {
+                    users.forEach(user -> {log.info(user.getName());});
+                    allUsers.addAll(users);
+                }
+        );
+        log.info("allUsers: {},distinct Users:{}", allUsers.size(), allUsers.stream().map(User::getName).distinct().count());
+    }
+
 }
