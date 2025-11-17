3c74b7a5199ca91c8a60d28b34149bd0c681348a
Meta39 <53995537@qq.com>
2025-11-17 13:08:21 +0800

    SpringBoot测试定时任务和异步线程

---
+++ spring-boot-demo/src/main/java/com/fu/springbootdemo/scheduling/SchedulingTest.java
@@ -0,0 +1,21 @@
+package com.fu.springbootdemo.scheduling;
+
+import com.fu.springbootdemo.async.AsyncThread;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SchedulingTest {
+    private final AsyncThread asyncThread;
+
+    @Scheduled(fixedDelay = 5000L)
+    public void scheduled() {
+        log.info("scheduled invoke...");
+        asyncThread.asyncThread();
+    }
+
+}
